8f65c5baf5959376df0d16b57e8a5f9d0a494165
Jeremy Moormann <jmoormann@sbgtv.com>
2025-07-17 11:49:23 -0400

    condense

--- index.php
+++ index.php
@@ -2,14 +2,10 @@
 
 require_once 'vendor/autoload.php';
 
-use GingerTek\Routy;
-use App\Middlewares\AuthMiddleware;
-use App\Controllers\ApiController;
-
 \App\Services\Env::load(__DIR__ . '/.env');
 
-$app = new Routy;
-$app->use(AuthMiddleware::index(...));
-$app->group('/api', ApiController::index(...));
+$app = new \GingerTek\Routy;
+$app->use(\App\Middlewares\AuthMiddleware::index(...));
+$app->group('/api', \App\Controllers\ApiController::index(...));
 $app->serveStatic('/', 'public');
 $app->end(404);
